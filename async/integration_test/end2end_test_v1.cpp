aa97f2b1224675ce20d5f43746512037dc4cc7d9
sunjiahe <975759105@qq.com>
2021-03-13 23:05:55 +0800

    Add end2end test

--- async/integration_test/end2end_test_v1.cpp
+++ async/integration_test/end2end_test_v1.cpp
@@ -1,10 +1,54 @@
+#include <chrono>
+#include <cstdio>
+#include <string>
+#include <thread>
 #include "async/runtime/graph.h"
 #include "async/context/kernel_frame.h"
 #include "async/context/host_context.h"
+#include "async/context/chain.h"
+#include "async/support/ref_count.h"
 
 using namespace ficus;
 using namespace async;
+using namespace std::chrono;
+
+int LargeComputeFn(int num) {
+    float res;
+    for (int i = 0; i < num; ++i) {
+        res += (float)i  / 10.2;
+    }
+    return res;
+}
 
 int main() {
-    auto runContext = CreateCustomHostContext(20, 1);
+    std::cout << "hardware concurrency number! " << std::thread::hardware_concurrency() << "\n";
+    auto runContext = CreateCustomHostContext(std::thread::hardware_concurrency(), 1);
+    AsyncGraph* memory = runContext->Allocate<AsyncGraph>();
+    AsyncGraph* graphOri = new (memory) AsyncGraph(runContext.get());
+    RCReference<AsyncGraph> graph = TakeRef(graphOri);
+    graph->emplace_back({}, {"output"}, [](async::CommonAsyncKernelFrame* frame){
+        });
+    for (int i = 0; i < 100; ++i) {
+        graph->emplace_back({"output"}, {"result" + std::to_string(i)}, [](async::CommonAsyncKernelFrame* frame){
+            LargeComputeFn(frame->GetArgAt<int>(0));
+            frame->EmplaceResult<async::Chain>();
+        });
+    }
+    graph->BuildGraph();
+
+    int numIters = 1;
+    int iterRangeValue = 100000;
+    std::vector<std::vector<RCReference<AsyncValue>>> results;
+    results.resize(numIters + 1);
+    results[0].push_back(runContext->MakeAvailableAsyncValueRef<int>(iterRangeValue));
+    auto start = high_resolution_clock::now();
+    for (int i = 0; i < numIters; ++i) {
+        RunAsyncGraph(graph.get(), results[i], results[i+1], false);
+    }
+    runContext->Await(results[numIters]);
+    // for (int i = 0; i < numIters * 100; ++i) {
+    //     LargeComputeFn(iterRangeValue);
+    // }
+    auto end =high_resolution_clock::now();
+    std::cout << duration_cast<nanoseconds>(end - start).count();
 }
