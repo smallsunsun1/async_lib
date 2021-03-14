6c4190cbc6439ec6f0c3478de7c9a6d7ea9a130f
sunjiahe <975759105@qq.com>
2021-03-14 21:52:01 +0800

    start to add some coroutine code

--- async/coroutine/coroutine_test.cpp
+++ async/coroutine/coroutine_test.cpp
@@ -3,6 +3,8 @@
 #include <stdexcept>
 #include <thread>
 #include <cassert>
+
+#include "task.h"
  
 auto switch_to_new_thread(std::jthread& out) {
   struct awaitable {
@@ -38,8 +40,33 @@ task resuming_on_new_thread(std::jthread& out) {
   // awaiter destroyed here
   std::cout << "Coroutine resumed on thread: " << std::this_thread::get_id() << '\n';
 }
+
+struct TestAwaitable {
+  bool await_ready() {return false;}
+  std::coroutine_handle<> await_suspend(std::coroutine_handle<> h) {
+    return h;
+  }
+  int await_resume() {return 100;}
+};
+
+using namespace ficus;
+using namespace async;
+
+Task<int> TaskTest() {
+  std::cout << "hello world!\n";
+  int n = co_await TestAwaitable{};
+  std::cout << "hello world!\n";
+  co_return n;
+}
+
+Task<> Example() {
+  Task<int> countTask = TaskTest();
+  int result = co_await countTask;
+  std::cout << "final count = "  << result << std::endl;
+}
  
 int main() {
-  std::jthread out;
-  resuming_on_new_thread(out);
+  // std::jthread out;
+  // resuming_on_new_thread(out);
+  Example();
 }
