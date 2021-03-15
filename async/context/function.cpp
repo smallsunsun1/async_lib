b07cb20672e76042bcc6c9a0c4eb95b1388f0273
孙嘉禾 <jiahe.sun@yitu-inc.com>
2021-03-15 14:09:33 +0800

    Add Execute Function Wrapper For async::Function

--- async/context/function.cpp
+++ async/context/function.cpp
@@ -3,13 +3,22 @@
 namespace ficus {
 namespace async {
 
-void Function::Execute(const Function* func, std::vector<RCReference<AsyncValue>>& arguments, std::vector<RCReference<AsyncValue>>& results, HostContext* ctx) {
+void Function::Execute(const Function* func, std::vector<RCReference<AsyncValue>>& arguments, std::vector<RCReference<AsyncValue>>& results, HostContext* ctx, 
+        bool blockedTask) {
     absl::InlinedVector<AsyncValue*, 4> argumentsPtr;
     argumentsPtr.reserve(arguments.size());
     for (auto& elem: arguments) {
         argumentsPtr.push_back(elem.get());
     }
-    func->Execute(absl::MakeConstSpan(argumentsPtr.data(), argumentsPtr.size()), absl::MakeSpan(results.data(), results.size()), ctx);
+    if (blockedTask) {
+        ctx->EnqueueBlockingWork([ctx, &argumentsPtr, &results, func](){
+            func->Execute(absl::MakeConstSpan(argumentsPtr.data(), argumentsPtr.size()), absl::MakeSpan(results.data(), results.size()), ctx);
+        });
+    } else {
+        ctx->EnqueueWork([ctx, &argumentsPtr, &results, func](){
+            func->Execute(absl::MakeConstSpan(argumentsPtr.data(), argumentsPtr.size()), absl::MakeSpan(results.data(), results.size()), ctx);
+        });
+    }
 }
 
 }
