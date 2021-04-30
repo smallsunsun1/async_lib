eb87fc4d89f94b770935d63d3641a8dfd1cc6b43
sunjiahe <975759105@qq.com>
2021-04-30 14:29:33 +0800

    [Test] add openmp task depend test

--- async/integration_test/openmp_function_test.cpp
+++ async/integration_test/openmp_function_test.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
+#include <thread>
+#include <chrono>
+#include <cassert>
 
 void LoopSimdTest() {
   std::vector<int> data(100);
@@ -38,9 +41,43 @@ for (int i = 0; i < 10; ++i) {
 }
 }
 
+void TaskDependTest() {
+int x = 0;
+int y = 5;
+#pragma omp parallel
+{
+#pragma omp single nowait
+{
+#pragma omp task depend(out: x)
+{
+  x = 1;
+}
+#pragma omp task depend(in: x) depend(out: y)
+{
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  y = y + 1;
+}
+#pragma omp task depend(inout: x)
+{
+  x++;
+  assert(x == 2);
+}
+#pragma omp task depend(in: x, y) 
+{
+  std::cout << x + y << std::endl;
+}
+}
+}
+}
+
 int main() {
   omp_set_num_threads(4);
+  std::cout << "LoopCollapseTest Start" << std::endl;
   LoopCollapseTest();
+  std::cout << "LoopSimdTest Start" << std::endl;
   LoopSimdTest();
+  std::cout << "ParallelTaskTest Start" << std::endl;
   ParallelTaskTest();
+  std::cout << "TaskDependTest Start" << std::endl;
+  TaskDependTest();
 }
