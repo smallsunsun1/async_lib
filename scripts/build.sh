98d2506935489ea4bca5c0093d372353f4dda691
sss <975759105@qq.com>
2021-08-09 07:42:50 +0800

    [Update] change default compile options to optimized mode

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
 # bazel build //... --define=malloc=tcmalloc
-bazel build //... --define=malloc=normal #--define=use_cxx_20=on
+bazel build --config=optimize //... --define=malloc=normal --define=use_cxx_20=on
